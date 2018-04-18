74c6ba3c936339c8e918d7544569c70b50a3800c
Balázs Kiss (Forest) <balazs.kiss@webvalto.hu>
2018-04-18 17:01:56 +0200

    Very basic implementation of the problem

--- simplex-solver-feri/src/app/app.component.ts
+++ simplex-solver-feri/src/app/app.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  simplex = require('simplex-solver');
+
+  line1 = '3x + 2y + z <= 10';
+  line2 = '2x + 5y + 3z <= 15';
+  x = 2;
+  y = 3;
+  z = 4;
+  result = null;
+  base = null;
+
+  constructor(){}
+
+  changed(event, valtozoNev){
+    console.log("változás:", event, valtozoNev)
+    this.calculate();
+  }
+
+  calculate(){
+    let inputs = this.x + "x + " + this.y + "y + " + this.z + "z";
+    console.log("to find: ", inputs);
+    this.result = this.simplex.maximize(inputs, [
+      this.line1,
+      this.line2
+    ]);
+
+    this.base = JSON.stringify(this.result.tableaus);
+
+    console.log("result:", this.result);
+  }
 }
