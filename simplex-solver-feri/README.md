bdfc4b8fee83c325d7b95b9c88287df00370fa56
Balázs Kiss (Forest) <balazs.kiss@webvalto.hu>
2018-04-18 16:20:21 +0200

    Renamed project

--- simplex-solver-feri/README.md
+++ simplex-solver-feri/README.md
@@ -1,4 +1,4 @@
-# SimplexSolver
+# SimplexSolverFeri
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.7.3.
 
--- simplex-solver-feri/e2e/app.e2e-spec.ts
+++ simplex-solver-feri/e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('simplex-solver App', () => {
+describe('simplex-solver-feri App', () => {
   let page: AppPage;
 
   beforeEach(() => {
